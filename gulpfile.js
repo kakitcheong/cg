b9f602b2acc2685735b75987a4964cbea8268c9f
slg-ice <slg-ice@slg-ice.local>
2016-11-22 12:51:11 +0800

    Concatenated Javascript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	count = require('gulp-count'),
+	concat = require('gulp-concat');
 
 gulp.task('log', function(){
-	gutil.log('checking');
+	for(i = 0; i < jsSources.length; i++){
+		gutil.log(jsSources[i]);	
+	}
 });
+
+gulp.task('js', function(){
+	gulp.src('components/scripts/*.js')
+		.pipe(count('## js-files selected'))
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'))
+});
+
+
