c591d235f020a7ac1c4eb6e47cbc2e86a4ba2ea9
slg-ice <slg-ice@slg-ice.local>
2016-11-22 13:11:50 +0800

    Added Browserify

---
+++ components/scripts/template.js
@@ -0,0 +1,4 @@
+$(function(){
+	var Pug = require('pug');
+	console.log('Pug is up and running');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	count = require('gulp-count'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
 gulp.task('log', function(){
 	for(i = 0; i < jsSources.length; i++){
@@ -13,6 +14,7 @@ gulp.task('js', function(){
 	gulp.src('components/scripts/*.js')
 		.pipe(count('## js-files selected'))
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
 
