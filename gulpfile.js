e38185bbb0ce4dad23a9dced55612bd1b13a4486
slg-ice <slg-ice@slg-ice.local>
2016-11-22 17:45:15 +0800

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	count = require('gulp-count'),
 	concat = require('gulp-concat'),
 	browserify = require('gulp-browserify'),
+	connect = require('gulp-connect'),
 	compass = require('gulp-compass');
 
 var sassSources = ['components/sass/style.scss'];
@@ -20,6 +21,7 @@ gulp.task('js', function(){
 		.pipe(browserify())
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/js'))
+		.pipe(connect.reload())
 });
 
 gulp.task('compass', function(){
@@ -35,11 +37,31 @@ gulp.task('compass', function(){
 		}))
 		.on('error', gutil.log)
 		.pipe(gulp.dest('builds/development/stylesheets'))
+		.pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
 	gulp.watch('components/scripts/*.js', ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch('builds/development/*.html', ['html']);
+	gulp.watch('builds/development/js/*.json', ['json']);
 });
 
-gulp.task('default', ['js', 'compass', 'watch']);
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development',
+		livereload: true
+	});
+});
+
+gulp.task('html', function(){
+	gulp.src('builds/development/*.html')
+		.pipe(connect.reload());
+});
+
+gulp.task('json', function(){
+	gulp.src('builds/development/js/*.json')
+		.pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'js', 'compass', 'connect', 'watch']);
