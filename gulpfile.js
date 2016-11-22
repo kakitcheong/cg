12a3c6897e52bb03622021f587b17e88c298f01f
slg-ice <slg-ice@slg-ice.local>
2016-11-22 18:20:43 +0800

    Added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,22 @@ var gulp = require('gulp'),
 	connect = require('gulp-connect'),
 	compass = require('gulp-compass');
 
-var sassSources = ['components/sass/style.scss'];
+var env,
+	sassSources,
+	outputDir,
+	sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env==='development'){
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+} else{
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+sassSources = ['components/sass/style.scss'];
 
 gulp.task('log', function(){
 	for(i = 0; i < jsSources.length; i++){
@@ -20,7 +35,7 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/js'))
+		.pipe(gulp.dest(outputDir + 'js'))
 		.pipe(connect.reload())
 });
 
@@ -29,22 +44,22 @@ gulp.task('compass', function(){
 		.pipe(compass({
 			css: 'stylesheets',
 			sass: 'components/sass',
-			image: 'builds/development/images',
-			style: 'expanded',
+			image: outputDir + 'images',
+			style: sassStyle,
 			require: [
 				'modular-scale'
 			]
-		}))
-		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/stylesheets'))
+		})
+		.on('error', gutil.log))
+		.pipe(gulp.dest(outputDir + 'stylesheets'))
 		.pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
 	gulp.watch('components/scripts/*.js', ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
-	gulp.watch('builds/development/*.html', ['html']);
-	gulp.watch('builds/development/js/*.json', ['json']);
+	gulp.watch(outputDir + '*.html', ['html']);
+	gulp.watch(outputDir + 'js/*.json', ['json']);
 });
 
 gulp.task('connect', function(){
@@ -55,12 +70,12 @@ gulp.task('connect', function(){
 });
 
 gulp.task('html', function(){
-	gulp.src('builds/development/*.html')
+	gulp.src(outputDir+ '*.html')
 		.pipe(connect.reload());
 });
 
 gulp.task('json', function(){
-	gulp.src('builds/development/js/*.json')
+	gulp.src(outputDir + 'js/*.json')
 		.pipe(connect.reload());
 });
 
